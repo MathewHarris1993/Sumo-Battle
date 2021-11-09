f796eaed4235661face206a5c39f7e37eeda7f22
Mathew Harris <10959101@uvu.edu>
2021-11-09 13:24:57 -0700

    Spawn Random
    Coded in a random spawner.

--- Sumo Battle1/Assets/Scripts/SpawnManager.cs
+++ Sumo Battle1/Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,18 @@ public class SpawnManager : MonoBehaviour
 
     //variables
     public GameObject enemyPrefab;
-
+    private float spawnRange = 9;
 
     // Start is called before the first frame update
     void Start()
     {
+        //spawn location random
+        float spawnPosx = Random.Range(-spawnRange, spawnRange);
+        float spawnPosy = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosx, 0, spawnPosy);
+
+
+        //spawn enemy
         Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
         
     }
