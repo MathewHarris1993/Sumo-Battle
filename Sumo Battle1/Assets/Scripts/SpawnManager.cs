2170a134b0d26e1861eb74b02a3e92c1d128bbbc
Mathew Harris <10959101@uvu.edu>
2021-11-09 14:33:35 -0700

    Respawn PowerUp
    Set powerups to spawn at the end of each wave with incremental numbers.

--- Sumo Battle1/Assets/Scripts/SpawnManager.cs
+++ Sumo Battle1/Assets/Scripts/SpawnManager.cs
@@ -9,21 +9,39 @@ public class SpawnManager : MonoBehaviour
     public GameObject enemyPrefab;
     private float spawnRange = 9;
     public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerUpPrefab;
+    public int powerUpCount;
+    public int powerUpNumber = 1;
+
 
     // Start is called before the first frame update
     void Start()
     {
         //spawn enemy
-        SpawnEnemyWaves(1);        
+        SpawnEnemyWaves(waveNumber);
+        SpawnPowerUp(powerUpNumber);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        powerUpCount = FindObjectsOfType<PowerUp>().Length;
         enemyCount = FindObjectsOfType<Enemy>().Length;
         if(enemyCount == 0)
         {
-            SpawnEnemyWaves(1);
+            waveNumber++;
+            SpawnEnemyWaves(waveNumber);
+            powerUpNumber++;
+            SpawnPowerUp(powerUpNumber);
+        }
+    }
+    void SpawnPowerUp(int powerUpSpawn)
+    {
+        for(int i = 0; i < powerUpSpawn; i++)
+        {
+            Instantiate(powerUpPrefab, GenerateSpawn(), powerUpPrefab.transform.rotation);
         }
     }
 
