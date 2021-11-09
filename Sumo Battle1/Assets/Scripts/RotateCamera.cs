8a8f4d74f14fb8f242ce0c875593e5593c04d1e1
Mathew Harris <10959101@uvu.edu>
2021-11-09 12:40:02 -0700

    Camera Controls
    Coded in control to spin camera

--- Sumo Battle1/Assets/Scripts/RotateCamera.cs
+++ Sumo Battle1/Assets/Scripts/RotateCamera.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+
+    //rotate variable
+    public float roatationSpeed = 50;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class RotateCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //rotation input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * roatationSpeed * Time.deltaTime);
+
+
+
     }
 }
